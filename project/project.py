9c85af645b576e7a1792b73053449ae3114ecc20
paddywej <paddy.wej@gmail.com>
2025-03-28 01:43:24 +0700

    added year files

--- project/project.py
+++ project/project.py
@@ -2,9 +2,13 @@ import reflex as rx
 from rxconfig import config
 from project.components.NavBar import navbar
 from project.components.SearchBar import search_bar
-from project.pages.homepage import menu_year  # Ensure lowercase file name
+from project.pages.homepage import menu_year
 from project.pages.register import register
 from project.pages.login import login
+from project.pages.year1 import Year1
+from project.pages.year2 import Year2
+from project.pages.year3 import Year3
+from project.pages.year4 import Year4
 
 style = {
     "::placeholder": {
@@ -43,6 +47,27 @@ def index() -> rx.Component:
         bg="white"
     )
 
+def year1() -> rx.Component:
+    return rx.container(
+        rx.vstack(
+            navbar(), 
+            Year1(),
+        ),
+        bg="white"
+        
+    )
+# def year2() -> rx.Component:
+#     return rx.container(
+#         Year2(),
+#     )
+# def year3() -> rx.Component:
+#     return rx.container(
+#         Year3(),
+#     )
+# def year4() -> rx.Component:
+#     return rx.container(
+#         Year4(),
+#     )
 app = rx.App(style=style)
 
 # Add all pages properly
@@ -50,3 +75,4 @@ app.add_page(index, route="/")
 app.add_page(register_page, route="/register")
 app.add_page(login_page, route="/login")
 app.add_page(HomePage, route="/home")
+app.add_page(year1, route="/year1")
